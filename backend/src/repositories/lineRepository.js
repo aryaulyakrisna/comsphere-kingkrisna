b19357b666084dedb6cc13992672aed154276c66
aryaulyakrisna117 <aryaulyakrisna@gmail.com>
2025-10-03 09:27:45 +0700

    fix: lines

--- backend/src/repositories/lineRepository.js
+++ backend/src/repositories/lineRepository.js
@@ -7,7 +7,7 @@ export const line = async (
   let res = {};
   if (cl) {
     const [existingClLines] = await pool.query(
-      "SELECT cl_line_name, crossed_stations transit_stations, cl_line_code FROM t_cl_line",
+      "SELECT cl_line_name, crossed_stations, transit_stations, cl_line_code FROM t_cl_line",
     );
 
     res.cl_lines = existingClLines;
@@ -15,7 +15,7 @@ export const line = async (
 
   if (lrt) {
     const [existingLrtLines] = await pool.query(
-      "SELECT lrt_line_name, crossed_stations transit_stations, lrt_line_code FROM t_lrt_line"
+      "SELECT lrt_line_name, crossed_stations lrt_line_code FROM t_lrt_line"
     );
 
     res.lrt_lines = existingLrtLines;
